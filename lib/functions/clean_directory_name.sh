4878dd9fd71e49e717c65bb2e53817f0d1910e66
Josh Parkinson <joshparkinson1991@gmail.com>
2020-03-30 16:19:32 +0100

    Add missing comment

--- lib/functions/clean_directory_name.sh
+++ lib/functions/clean_directory_name.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Cleans a directory name removing any metadata about the source file format
+#
+# For example, if using the source file directory name to drive the transcode output directory name ideally you do
+# not want to be mixing meta descriptions. For example, if the source directory name contains the fact the files stored
+# within it are 'FLAC 24Bit', you do not want this to be carried across to a transcode output directory as that directory
+# does not store files of that type.
+#
+# This function removes all file type/format meta from a directory name so that it can be used safely in naming of
+# transcode directories.
+#
+# Usage:
+# clean_directory_name <directory name>
+# VARIABLE=$(clean_directory_name <directory name>)
 clean_directory_name() {
     echo $(sed '
         s|flac||g;
