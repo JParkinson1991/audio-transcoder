992f77a2350e6761c2b56784fb102bd8b2d240c3
Josh Parkinson <joshparkinson1991@gmail.com>
2020-03-30 16:19:40 +0100

    Add missing usage statements

--- lib/functions/status_messages.sh
+++ lib/functions/status_messages.sh
@@ -2,15 +2,25 @@
 # Requires use of ./lib/util/format.sh if used outside of the application binary
 
 # Outputs a standardised error message
+#
+# Usage:
+# error <message>
 function error() {
     echo -e "${RED}Error:${RESET} $1"
 }
 
 # Outputs a standardised success message
+#
+# Usage:
+# success <message>
 function success() {
     echo -e "${GREEN}Success:${RESET} $1"
 }
 
+# Outputs a standardised notice message
+#
+# Usage:
+# notice <message>
 function notice() {
     echo -e "${BLUE}Notice:${RESET} $1"
 }
